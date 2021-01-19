66ff7b1531c60752572c0c493a620a832b7a03f0
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-19 11:54:08 +0700

    add Class!

---
+++ textPost/LoadDiaryVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoadDiaryVC.swift
+//  textPost
+//
+//  Created by Bagus setiawan on 19/01/21.
+//
+
+import UIKit
+
+class LoadDiaryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
