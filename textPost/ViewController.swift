2723d8a1f539f09dcf6ec4f25881b7b57852887f
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-19 11:18:56 +0700

    add files !

---
+++ textPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  textPost
+//
+//  Created by Bagus setiawan on 19/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
