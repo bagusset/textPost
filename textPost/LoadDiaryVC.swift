7202ea76eae6bf14e30e02082a9b66ae76324c87
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-19 21:42:47 +0700

    retrive data from firebase database realtime to display in tableView!

--- textPost/LoadDiaryVC.swift
+++ textPost/LoadDiaryVC.swift
@@ -6,24 +6,66 @@
 //
 
 import UIKit
+import Firebase
 
 class LoadDiaryVC: UIViewController {
-
+  
+    @IBOutlet weak var loadTableView: UITableView!
+    
+    var ref : DatabaseReference!
+    
+    var postData = [eventStruct]()
+    
+    struct eventStruct {
+            let title: String!
+            let date: String!
+            let note: String!
+        }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        loadTableView.delegate = self
+        loadTableView.dataSource = self
+        
+        loadData()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadData() {
+        ref = Database.database().reference()
+        ref.child("Notes").queryOrderedByKey().observe(.childAdded, with: { (DataSnapshot) in
+            if let valueDictionary  = DataSnapshot.value as? [AnyHashable: String]
+            {
+                let title = valueDictionary["Titles"]
+                let date  = valueDictionary["Dates"]
+                let note = valueDictionary["Note"]
+                self.postData.insert(eventStruct(title: title, date: date, note: note), at: 0)
+                self.loadTableView.reloadData()
+            }
+        })
+        
     }
-    */
+ 
 
+}
+
+extension LoadDiaryVC : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! LoadTableViewCell
+        cell.titleLabel.text = postData[indexPath.row].title
+        cell.dateLabel.text  = postData[indexPath.row].date
+        cell.noteLabel.text = postData[indexPath.row].note
+        
+        return cell
+        
+    }
+    
+    
 }
