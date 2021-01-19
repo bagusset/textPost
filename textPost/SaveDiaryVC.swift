c9fa9e0ee0ade8420f90af96322b071741d62ea5
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-01-19 15:48:19 +0700

    add IBOutlet !

--- textPost/SaveDiaryVC.swift
+++ textPost/SaveDiaryVC.swift
@@ -8,22 +8,25 @@
 import UIKit
 
 class SaveDiaryVC: UIViewController {
+    
+    @IBOutlet weak var inputTittleTextField: UITextField!
+    @IBOutlet weak var inputDateTextField: UITextField!
+    @IBOutlet weak var inputNoteTextfield: UITextField!
+    @IBOutlet weak var saveBtn: UIButton!
+    @IBOutlet weak var dissmisBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func dismissBtnPressed(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+        
     }
-    */
+    
 
 }
